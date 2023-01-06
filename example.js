09fb78c85395bd4e4570672a6172d1f79debe32e
manuel-heav <manuelheav32@gmail.com>
2023-01-06 11:12:40 +0300

    working on example.ts

--- example.js
+++ example.js
@@ -1,4 +1,11 @@
 // Type Annotations
+let myName = "Aman";
+let isCoolL = true;
+let myNum = 42;
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 4));
 // Inferred Types
 // Union Types
 // Objects and Interfaces
--- example.ts
+++ example.ts
@@ -1,5 +1,15 @@
 // Type Annotations
 
+let myName: string = "Aman";
+let isCoolL: boolean = true;
+let myNum: number = 42;
+
+function add(a : number,b: number)
+{
+    return a+b;
+}
+
+console.log(add(5, 4));
 // Inferred Types
 
 // Union Types
