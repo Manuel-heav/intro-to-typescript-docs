a1270f49ab563ec3f5449c7ba9c23ffe35ecb4e5
manuel-heav <manuelheav32@gmail.com>
2023-01-06 11:10:10 +0300

    working on example.ts

---
+++ example.js
@@ -0,0 +1,10 @@
+// Type Annotations
+// Inferred Types
+// Union Types
+// Objects and Interfaces
+// Optional Properties
+// Classes
+// Generics
+// any, unknown, never
+// Type Assertions
+// @ts-ignore / @ts-nocheck
