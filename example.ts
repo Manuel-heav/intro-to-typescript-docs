f9cace5f6ac5eb18a0638a31e2580108bd34d462
manuel-heav <manuelheav32@gmail.com>
2023-01-06 11:55:27 +0300

    updated file structure

--- example.ts
+++ example.ts
@@ -1,15 +1,15 @@
 // Type Annotations
 
-// let myName: string = "Aman";
-// let isCoolL: boolean = true;
-// let myNum: number = 42;
+    // let myName: string = "Aman";
+    // let isCoolL: boolean = true;
+    // let myNum: number = 42;
 
-// function add(a : number,b: number) :number
-// {
-//     return a+b;
-// }
+    // function add(a : number,b: number) :number
+    // {
+    //     return a+b;
+    // }
 
-// console.log(add(5, 4));
+    // console.log(add(5, 4));
 
 // Inferred Types
 
@@ -26,9 +26,44 @@ function add(a : number,b: number)
 
 console.log(add(5, 4));
 
+
+
+    // So basically let's have an array of numbers
+const numbers =  [1,2,34,4,6];
+    // This is a number array and if I try to push a string like
+
+    // numbers.push("Hi"); //This will get me an error because "Hi" is not a string which would have worked with vanilla js
+
 // Union Types
+    // What if we need to make an array that will contain both numbers and strings
+// const numbersAndStrings: (string | number)[] = [1,2,3, "Beamie", "Manuel"] -> This is the annotation but it since it's inferred we dont need to
+const numbersAndStrings = [1,2,3, "Beamie", "Manuel"];
+
+const theAnswer: string | number = "42";
+
+console.log(numbersAndStrings)
+
 
 // Objects and Interfaces
+
+interface Person{
+    name: string;
+    favNumber: number;
+}
+
+const person = {
+    name: "Manuel",
+    favNumber: 42,
+};
+
+function greet(person: Person){
+    return  `Hello ${person.name}`;
+}
+
+console.log(greet({
+    name: "Manuel",
+    favNumber: 42,
+}))
 
 // Optional Properties
 
